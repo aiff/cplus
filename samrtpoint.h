6efc9da3a103526c35bb09f3fdc021ee981c3422
eric <aiff@outlook.com>
2017-12-01 14:35:05 +0800

    inti commit

---
+++ samrtpoint.h
@@ -0,0 +1,19 @@
+#ifndef SAMRTPOINT_H
+#define SAMRTPOINT_H
+namespace datastr {
+
+template <typename T>
+class smartpointer
+{
+
+protected:
+    T* m_point;
+public:
+    smartpointer()
+    {
+
+    }
+
+}
+}
+#endif // SAMRTPOINT_H
